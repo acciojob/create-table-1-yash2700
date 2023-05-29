b77c45a4fffec7199256828076df56f3c89c5541
BOGGALA YASWANTH REDDY <93768814+yash2700@users.noreply.github.com>
2023-05-29 14:13:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function insert_Row() {
-    //Write your code here
+  let table=document.getElementById("sampleTable");
+	let firstRow = table.rows[0];
+let row = document.createElement("tr");
+row.innerHTML="<td>New Cell1</td>" + "<td>New Cell2</td>"
   
+	table.appendChild(row)
+
+	
+table.insertBefore(row,table.firstChild);
   
 }
